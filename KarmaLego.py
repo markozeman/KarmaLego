5c7c5d2db02c31743a9b0764da31a9c2eedfde91
markozeman <mz4730@student.uni-lj.si>
2019-09-03 16:12:58 +0200

    Temporal relations written

--- KarmaLego.py
+++ KarmaLego.py
@@ -10,6 +10,28 @@ Link: https://pdfs.semanticscholar.org/a800/83f16631756d0865e13f679c2d5084df03ae
 """
 
 
+class TIRP:
+    """
+    Representation of Time Interval Relation Pattern (TIRP) with two lists.
+    Implementation of basic methods to work with TIRPs.
+    """
+    def __init__(self):
+        pass
+
+    def extend(self, label):
+        pass
+
+        # check size after extending
+
+    def check_size(self):
+        """
+        Check if length of list relations is right regarding length of list labels.
+
+        :return: boolean - if size of lists labels and relations match
+        """
+
+
+
 class Karma:
     """
     Implementation of Karma part of KarmaLego algorithm.
@@ -42,4 +64,11 @@ class KarmaLego:
 if __name__ == "__main__":
     entity = entity_list[0]
     plot_entity(entity)
+
+
+
+
+    # todo
+    # kako pametno predstaviti TIRP?  Class TIRP
+    # funkcija za vertical support s thresholdom
 
--- helpers.py
+++ helpers.py
@@ -26,3 +26,62 @@ def plot_entity(entity):
     plt.show()
 
 
+def lexicographic_sorting(entity):
+    """
+    Lexicographically order entity. Sorted function orders tuples first by the first element, then by the second, etc.
+
+    :param entity: dict - key: state, value: list of time intervals of specific event
+    :return: list with lexicographically ordered time intervals
+    """
+    return sorted([(*ti, state) for state, time_intervals in entity.items() for ti in time_intervals])
+
+
+def temporal_relations(ti_1, ti_2, epsilon, max_distance):
+    """
+    Find out the temporal relation between time intervals ti_1 and ti_2 among 7 Allen's temporal relations.
+    It is assumed that ti_1 is lexicographically before or equal to ti_2 (ti_1 <= ti_2).
+
+    :param ti_1: first time interval (A.start, A.end)
+    :param ti_2: second time interval (B.start, B.end)
+    :param epsilon: maximum amount of time between two events that we consider it as the same time
+    :param max_distance: maximum distance between two time intervals that means first one still influences the second
+    :return: string - one of 7 possible temporal relations
+    """
+    A_start, A_end = ti_1
+    B_start, B_end = ti_2
+    if epsilon < B_start - A_end < max_distance:    # before
+        return '<'
+    elif abs(B_start - A_end) <= epsilon:   # meets
+        return 'm'
+    elif B_start - A_start > epsilon and A_end - B_start > epsilon and B_end - A_end > epsilon:     # overlaps
+        return 'o'
+    elif B_start - A_start > epsilon and A_end - B_end > epsilon:   # contains
+        return 'c'
+    elif B_start - A_start > epsilon and abs(B_end - A_end) <= epsilon:     # finish by
+        return 'fi'
+    elif abs(B_start - A_start) <= epsilon and abs(B_end - A_end) <= epsilon:   # equal
+        return '='
+    elif abs(B_start - A_start) <= epsilon and B_end - A_end > epsilon:     # starts
+        return 's'
+    else:
+        print('Wrong temporal relation!')
+        return '?'
+
+
+
+
+
+
+def vertical_support(entity_list, TIRP, min_ver_supp):
+    """
+    Check if TIRP is present in at least min_ver_supp entities.
+
+    :param entity_list: list of all entities
+    :param TIRP: time interval relation pattern
+    :param min_ver_supp: proportion (value between 0-1) defining threshold for accepting TIRP
+    :return: boolean - True if given TIRP has at least min_ver_supp support, otherwise False
+    """
+
+
+
+
